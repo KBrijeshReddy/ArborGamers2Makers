358235c74653200e26547d6f74c6d3a06b3a41eb
KBrijeshReddy <brijeshreddy12@gmail.com>
2024-11-22 21:06:45 +0530

    camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField] private Transform target;
+
+    void Update()
+    {
+        Vector3 newPos = target.position + offset;
+        newPos.y = 0f;
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
+    }
+
+}
